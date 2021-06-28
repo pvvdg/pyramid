d723e5b4deecd0448894a90646c35d713c77bc87
pvvdg <vladpetrachuk@gmail.com>
2021-06-28 18:08:59 +0300

    upd: compress the program

--- main.go
+++ main.go
@@ -5,24 +5,13 @@ import (
 )
 
 func main() {
-	symbol := "@"
 	n := 0
 	fmt.Scan(&n)
-	var matrix [][]string
-	var row []string
-
-	for i := 0; i < n; i++ {
-		for j := 0; j < n*2-1; j++ {
-			row = append(row, " ")
-		}
-		matrix = append(matrix, row)
-		row = []string{}
-	}
-
-	for i := 0; i < len(matrix); i++ {
-		for j := 0; j < len(matrix[i]); j++ {
-			if j >= ((len(matrix)-1)-i) && (j <= (len(matrix[i])-1)-((len(matrix)-1)-i)) {
-				fmt.Print(symbol)
+	lenMatrix, lenRow := n, n*2-1
+	for i := 0; i < lenMatrix; i++ {
+		for j := 0; j < lenRow; j++ {
+			if j >= ((lenMatrix-1)-i) && (j <= (lenRow-1)-((lenMatrix-1)-i)) {
+				fmt.Print("@")
 			} else {
 				fmt.Print(" ")
 			}
